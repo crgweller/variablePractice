c1a527cdde66278886ed80fafdb4e2a0e2592c3b
Craig Weller <crg.weller@gmail.com>
2022-09-21 10:34:48 -0600

    Add wednesday update

--- variables.js
+++ variables.js
@@ -1,6 +1,17 @@
 var email = "example@gmail.com"
 var age = 37
 var understanding = false
+var numberOfCats = 0
+var numberOfDogs = 1
 console.log(email)
 console.log(age)
 console.log(understanding)
+console.log(numberOfCats)
+console.log(numberOfDogs)
+var age = 38
+var email = "otherexample@gmail.com"
+console.log(email)
+console.log(age)
+console.log(understanding)
+console.log(numberOfCats)
+console.log(numberOfDogs)
