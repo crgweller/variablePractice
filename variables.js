f05a8ef0e2f8fa90db7f3b53fe5a0b37d3780b42
Craig Weller <crg.weller@gmail.com>
2022-09-21 10:48:04 -0600

    Do it right this time

--- variables.js
+++ variables.js
@@ -1,15 +1,8 @@
-var email = "example@gmail.com"
-var age = 37
+var email = "otherexample@gmail.com"
+var age = 38
 var understanding = false
 var numberOfCats = 0
 var numberOfDogs = 1
-console.log(email)
-console.log(age)
-console.log(understanding)
-console.log(numberOfCats)
-console.log(numberOfDogs)
-var age = 38
-var email = "otherexample@gmail.com"
 console.log(email)
 console.log(age)
 console.log(understanding)
